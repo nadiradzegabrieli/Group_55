d06734c690fd55cfff06dfe2f69102c765ee6f20
nadiradzegabrieli <nadiradzegabriel6>
2025-04-24 21:49:06 +0400

    sixsth commit

---
+++ level 55/homework/homework.py
@@ -0,0 +1,12 @@
+# 1 Codewars Done In Classwork
+# 2 Codewars Done In Classwork
+# 3 Codewars Done In Classwork
+# 4 Codewars Done In Classwork
+
+# 5 Codewars
+
+def get_size(w, h, d):
+    return [2 * (w * h + h * d + w * d), w * h * d]
+
+# Example usage:
+print(get_size(2, 3, 4))  # Output: [52, 24]
